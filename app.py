f6da19c562d5c376a1f9d0709975b07e36ea31b3
Dave Borowski <dmborowski@msn.com>
2020-11-18 20:13:25 -0700

    Adding final destination for flask app

--- app.py
+++ app.py
@@ -28,6 +28,7 @@ def home():
         f"To get the transactions by date: /transaction<br/>"
         f"To get a list of customers: /customer<br/>"
         f"To get the product category defs: /category<br/>"
+        f"To get all data: /alldata<br/>"
     )
     
 @app.route("/transaction")
@@ -35,7 +36,8 @@ def transaction():
 
     
     results = engine.execute('select * from `transaction`').fetchall()
-    results_list=[{'id': result[0], 'transaction_id': result[1], 'cust_id': result[2], 'tran_date': result[3], 'pro_subcat_code': result[4], 'pro_cat_code': result[5], 'Qty': result[6], 'Rate': result[7], 'Tax': result[8], 'total_amt': result[9], 'Store_type': result[10]} for result in results]
+    results_list=[{'id': result[0], 'transaction_id': result[1], 'cust_id': result[2], 'tran_date': result[3], 'pro_subcat_code': result[4], 
+    'pro_cat_code': result[5], 'Qty': result[6], 'Rate': result[7], 'Tax': result[8], 'total_amt': result[9], 'Store_type': result[10]} for result in results]
 
     return jsonify(results_list)
 
@@ -53,6 +55,16 @@ def category():
 
     results = engine.execute('select * from category').fetchall()
     results_list=[{'id': result[0], 'prod_cat_code': result[1], 'prod_cat': result[2], 'prod_sub_cat_code': result[3], 'prod_subcat': result[4]} for result in results]
+
+    return jsonify(results_list)
+
+@app.route("/alldata")
+def alldata():
+
+    results = engine.execute('select * from alldata').fetchall()
+    results_list=[{'id': result[0], 'transaction_id': result[1], 'cust_id': result[2], 'DOB': result[12], 'Gender': result[13], 'city_code': result[14], 
+    'tran_date': result[3], 'prod_cat_code': result[5], 'prod_cat': result[15], 'prod_subcat_code': result[4], 'prod_subcat': result[17], 
+    'Qty': result[6], 'Rate': result[7], 'Tax': result[8], 'total_amt': result[9], 'Store_type': result[10]} for result in results]
 
     return jsonify(results_list)
 
