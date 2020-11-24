e4519ec86c4280e834a600712d810d7a86f043dd
Dave Borowski <dmborowski@msn.com>
2020-11-23 17:09:10 -0700

    Added new columns to the db and updated flask

--- app.py
+++ app.py
@@ -13,9 +13,6 @@ engine = create_engine("sqlite:///test1.db")
 
 Base=automap_base()
 
-# class customer(Base):
-#     __tablename__ = 'customer' 
-
 Base.prepare(engine, reflect=True)
 
 session=Session(engine)
@@ -25,10 +22,10 @@ def home():
     """List all available api routes."""
     return (
         f"Available Routes:<br/>"
-        f"To get the transactions by date: /transaction<br/>"
-        f"To get a list of customers: /customer<br/>"
-        f"To get the product category defs: /category<br/>"
-        f"To get all data: /alldata<br/>"
+        f"To get the transactions by date: https://retail-sales-data.herokuapp.com/transaction<br/>"
+        f"To get a list of customers: https://retail-sales-data.herokuapp.com/customer<br/>"
+        f"To get the product category defs: https://retail-sales-data.herokuapp.com/category<br/>"
+        f"To get all data: https://retail-sales-data.herokuapp.com/alldata<br/>"
     )
     
 @app.route("/transaction")
@@ -37,7 +34,7 @@ def transaction():
     
     results = engine.execute('select * from `transaction`').fetchall()
     results_list=[{'id': result[0], 'transaction_id': result[1], 'cust_id': result[2], 'tran_date': result[3], 'pro_subcat_code': result[4], 
-    'pro_cat_code': result[5], 'Qty': result[6], 'Rate': result[7], 'Tax': result[8], 'total_amt': result[9], 'Store_type': result[10]} for result in results]
+    'pro_cat_code': result[5], 'Qty': result[6], 'Rate': result[7], 'Tax': result[8], 'total_amt': result[9], 'Store_type': result[10], 'Item': result[11], 'Sale/Return': result[12]} for result in results]
 
     return jsonify(results_list)
 
@@ -46,7 +43,7 @@ def customer():
 
     
     results = engine.execute('select * from customer').fetchall()
-    results_list=[{'id': result[0], 'customer_id': result[1], 'DOB': result[2], 'Gender': result[3], 'city_code': result[4]} for result in results]
+    results_list=[{'id': result[0], 'customer_id': result[1], 'DOB': result[2], 'Gender': result[3], 'city_code': result[4], 'city': result[5], 'state': result[6], 'zip': result[7], 'age': result[8]} for result in results]
 
     return jsonify(results_list)
 
@@ -62,9 +59,9 @@ def category():
 def alldata():
 
     results = engine.execute('select * from alldata').fetchall()
-    results_list=[{'id': result[0], 'transaction_id': result[1], 'cust_id': result[2], 'DOB': result[12], 'Gender': result[13], 'city_code': result[14], 
-    'tran_date': result[3], 'prod_cat_code': result[5], 'prod_cat': result[15], 'prod_subcat_code': result[4], 'prod_subcat': result[17], 
-    'Qty': result[6], 'Rate': result[7], 'Tax': result[8], 'total_amt': result[9], 'Store_type': result[10]} for result in results]
+    results_list=[{'id': result[0], 'transaction_id': result[1], 'cust_id': result[2], 'DOB': result[14], 'Gender': result[15], 'city_code': result[16], 'city': result[17], 
+    'state': result[18], 'zip': result[19],'tran_date': result[3], 'prod_cat_code': result[5], 'prod_cat': result[21], 'prod_subcat_code': result[4], 'prod_subcat': result[23], 
+    'Qty': result[6], 'Rate': result[7], 'Tax': result[8], 'total_amt': result[9], 'Store_type': result[10], 'Item': result[11], 'Sale/Return': result[12]} for result in results]
 
     return jsonify(results_list)
 
